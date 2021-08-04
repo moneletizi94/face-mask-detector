ad9e82fddc6e845da12bc7108493d735d5f58178
Andrea <petretiandrea@gmail.com>
2021-08-04 18:30:33 +0200

    first working version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import * as mask from './prediction'
 import * as utils from './utils'
 import * as tf from '@tensorflow/tfjs';
+import * as faceapi from 'face-api.js';
 //import * as faceapi from '@vladmandic/face-api/dist/face-api.esm.js';
 
 let video = document.getElementById("video");
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let status = document.getElementById("status");
 let model;
 
 function setupCamera() {
@@ -23,10 +25,13 @@ function setupCamera() {
 setupCamera();
 
 video.addEventListener('loadeddata', async () => {
-  console.log('loading models...')
+  status.innerText = "loading models..."
+  
   //await faceapi.loadSsdMobilenetv1Model('/models')
+  await faceapi.loadTinyFaceDetectorModel('/models')
+
   model = await mask.loadMaskModel()
-  console.log('loaded')
+  status.innerText = 'model loaded'
 
   gameLoop();
 });
@@ -45,17 +50,27 @@ async function gameLoop() {
     lastTimestmap = current;
     fps = Math.round(1 / secondsLeft);
 
-    //let detections = await faceapi.detectAllFaces(video);
-    //console.log(detections);
-
+    let detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
+    // filter detections by confidence value
+    // extract faces using faceapi
+    let faces = await faceapi.extractFaces(video, detections)
     //renderDetections(video, detections);
-    let pred = await mask.predict(model, video)
-    let label = LABELS[utils.arrayMaxIndex(pred)]
 
     ctx.drawImage(video, 0, 0, 600, 400);
+
+    if (faces.length > 0) {
+      let predictions = await mask.predict(model, faces[0])
+      console.log(predictions)
+      let label_pred = LABELS[utils.arrayMaxIndex(predictions)]
+      let box = detections[0].box
+      let box_pred = new faceapi.draw.DrawBox(box, { label: label_pred, lineWidth: 2 })
+      box_pred.draw(canvas)
+    }
+    
+    //faceapi.draw.drawDetections(canvas, detections);
     ctx.font = '25px Arial';
     ctx.fillStyle = 'black';
-    ctx.fillText(`FPS: ${fps}, ${label}`, 10, 30);
+    ctx.fillText(`FPS: ${fps}`, 10, 30);
 
     await new Promise(window.requestAnimationFrame);
 
